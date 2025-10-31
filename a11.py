91f99e063e408711fce5170e603841b235597ba9
JanviTayde <janvitayde19@gmail.com>
2025-10-31 09:26:03 -0700

    star commit

---
+++ a11.py
@@ -0,0 +1,9 @@
+#print() using specific data types.
+# {:d} is for int, {:f} is for float, {:s} for string
+
+rno = 200
+name = 'sakshi chinchkhede'
+per = 89.62
+
+print('Rno = {:d} Name = {:s} Per = {:.2f}'.format(rno,name,per))
+    
