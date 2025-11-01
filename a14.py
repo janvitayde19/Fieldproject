27f6aaa7e070c0026e492af281f95d7f90eea5b5
JanviTayde <janvitayde19@gmail.com>
2025-11-01 01:13:29 -0700

    first commit

---
+++ a14.py
@@ -0,0 +1,10 @@
+# for as collection loop
+colors =['green','blue','white','grey','pink','red']
+data =(2,3,5,7,4,4,3,6)
+
+for c in colors:
+    print(c,end='  ')
+
+for d in data:
+    print(d,end='  ')
+        
