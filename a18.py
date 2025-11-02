b83c202acea47b649e70d48e38eb1a524e2f9faa
JanviTayde <janvitayde19@gmail.com>
2025-11-02 03:05:45 -0800

    dictionries commit

---
+++ a18.py
@@ -0,0 +1,8 @@
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print (f'I owe the grocer ${grocery_bill:.2f}')
+
