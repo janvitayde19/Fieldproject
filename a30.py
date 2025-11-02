19bc97eef469890368bc2e6a41bb72011092094e
JanviTayde <janvitayde19@gmail.com>
2025-11-02 04:22:17 -0800

    Feet and Remaining Inches

---
+++ a30.py
@@ -0,0 +1,7 @@
+total_inches = 74
+
+feet = total_inches // 12  # Floor division for feet
+remaining_inches = total_inches % 12  # Modulo for remaining inches
+
+print(f"{total_inches} inches is equal to {feet} feet and {remaining_inches} inches.")
+
